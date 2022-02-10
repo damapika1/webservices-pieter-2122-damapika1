eaff8e3d39cd0cfef03558452cc2c48ffc58aec5
damapika <rayemin96@gmail.com>
2022-02-10 17:26:24 +0100

    create implemented

--- src/service/note.js
+++ src/service/note.js
@@ -3,7 +3,6 @@ const {
   getChildLogger
 } = require('../core/logging');
 const notesRepository = require('../repository/note');
-
 const debugLog = (message, meta = {}) => {
   if (!this.logger) this.logger = getChildLogger('note-service');
   this.logger.debug(message, meta);
@@ -13,31 +12,50 @@ const debugLog = (message, meta = {}) => {
 //   NOTES
 // } = require('../data/mock_data');
 
-const getAll = async (limit=100,offset=0) => {
+const getAll = async (limit = 100, offset = 0) => {
   debugLog('Fetching all notes');
-  const data = await notesRepository.findAll(limit,offset);
-  return {data:data,count:data.length,}
+  const data = await notesRepository.findAll(limit, offset);
+  return {
+    data: data,
+    count: data.length,
+  }
 }
 const getById = async (id) => {
   debugLog(`Fetching all notes with id: ${id}`);
   return notesRepository.findById(id);
 };
+
 const create = async ({
+  user,
   title,
   text,
   date
 }) => {
-
-  return new Promise((resolve) => {
-    const newNote = {
-      id: uuid.v4(),
-      title,
-      text,
-      date //:date.toISOString(),
-    };
-    NOTES = [...NOTES, newNote];
-    resolve(newNote);
+  debugLog('Creating new note', {
+    // user,
+    title,
+    text,
+    date
   });
+  // const { id: user_id } = await userService.register({ name: user });
+  return notesRepository.create({
+    title,
+    text,
+    date,
+    userId:'7f28c5f9-d711-4cd6-ac15-d13d71abff81',
+
+
+  });
+  // return new Promise((resolve) => {
+  //   const newNote = {
+  //     id: uuid.v4(),
+  //     title,
+  //     text,
+  //     date //:date.toISOString(),
+  //   };
+  //   NOTES = [...NOTES, newNote];
+  //   resolve(newNote);
+  // });
 };
 const updateById = async ({
   id,
@@ -45,17 +63,29 @@ const updateById = async ({
   text,
   date
 }) => {
-  return new Promise((resolve) => {
-    NOTES = NOTES.map((notes) => {
-      return notes.id === id ? {
-        ...notes,
-        title,
-        text,
-        date,
-      } : notes;
-    });
-    resolve(getById(id));
-  })
+  debugLog(`Updating a note with id ${id}`, {
+	title,
+  text,
+  date
+	});
+  return notesRepository.updateById({
+    id,
+    title,
+    text,
+    date,
+    userId:'7f28c5f9-d711-4cd6-ac15-d13d71abff81',
+  });
+  // return new Promise((resolve) => {
+  //   NOTES = NOTES.map((notes) => {
+  //     return notes.id === id ? {
+  //       ...notes,
+  //       title,
+  //       text,
+  //       date,
+  //     } : notes;
+  //   });
+  //   resolve(getById(id));
+  // })
 }
 const deleteById = async (id) => {
   debugLog(`Deleting a note with id ${id}`);
