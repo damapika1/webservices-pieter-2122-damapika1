fd295a95c89122d436698bfd44f82ec396558e01
Emin Rayme <rayme.emin@student.hogent.be>
2022-06-30 00:11:15 +0200

    auth

--- jest.config.js
+++ jest.config.js
@@ -27,7 +27,7 @@ module.exports = {
   ],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: '__test__/coverage',
+  coverageDirectory: '__tests__/coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -63,10 +63,10 @@ module.exports = {
   // forceCoverageMatch: [],
 
   // A path to a module which exports an async function that is triggered once before all test suites
-  globalSetup: './__test__/global.setup.js',
+  globalSetup: './__tests__/global.setup.js',
 
   // A path to a module which exports an async function that is triggered once after all test suites
-  globalTeardown: './__test__/global.teardown.js',
+  globalTeardown: './__tests__/global.teardown.js',
 
   // A set of global variables that need to be available in all test environments
   // globals: {},
@@ -158,15 +158,15 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[tj]s?(x)"
-    // '**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
+    // "**/__tests__/**/*.[jt]s?(x)",
+    //"**/?(*.)+(spec|test).[tj]s?(x)"
+    '**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     "\\\\node_modules\\\\",
-    "\\\\config\\\\"
+    // "\\\\config\\\\"
 
   ],
 
